76f7c943b635e9c76dd0e543809a07a21e6f3938
leoloewe <leohe@udel.edu>
2015-03-03 04:28:56 -0500

    quiz 2

--- src/Tuition/totaltuition.java
+++ src/Tuition/totaltuition.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class totaltuition{
 	
 	
-	public static void main(String[] args){
+	public static void main(String[] args){  
 		
 	Scanner input = new Scanner(System.in);
 		
